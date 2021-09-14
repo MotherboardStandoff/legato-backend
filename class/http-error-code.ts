aa7f644b49a16f34807314b058180e7f2e9242cf
MotherboardStandoff <jeroen.vanhoof87@gmail.com>
2021-09-14 10:40:31 +0200

    up and del to genre domain + additional Errorcode

--- class/http-error-code.ts
+++ class/http-error-code.ts
@@ -7,6 +7,8 @@ export function getHttpErrorCode(error: any): number {
             return 400;
         case ErrorCodes.NOT_FOUND:
             return 404;
+        case ErrorCodes.UNSUCCESSFUL:
+            return 500;
         default:
             return 500;
     }

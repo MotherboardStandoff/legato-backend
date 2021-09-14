33062591e7e632685a3114d5b95efe5c15fefabd
MotherboardStandoff <jeroen.vanhoof87@gmail.com>
2021-09-14 09:50:40 +0200

    Created genre domain save function

--- domain/genre-domain.ts
+++ domain/genre-domain.ts
@@ -1,10 +1,13 @@
-import { isUUID } from "class-validator";
+import { isUUID, length } from "class-validator";
 import { getCustomRepository } from "typeorm";
 import { Genre } from "../data/entity/genre";
 import { GenreRepository } from "../data/repository/genre-repository";
 import { ErrorCodes } from "../enum/error-codes";
 
 export class GenreDomain {
+
+    private readonly MIN_NAME_LENGTH: number = 1;
+    private readonly MAX_NAME_LENGTH: number = 255;
 
     constructor() { }
 
@@ -52,6 +55,23 @@ export class GenreDomain {
 
     public saveGenre(genreName: string): Promise<Genre> {
 
-        return new Promise(async (resolve, reject) => { });
+        return new Promise(async (resolve, reject) => {
+
+            try {
+
+                if (!length(genreName, this.MIN_NAME_LENGTH, this.MAX_NAME_LENGTH)) throw (ErrorCodes.INVALID);
+
+                let repo = getCustomRepository(GenreRepository);
+                let genre = new Genre(genreName);
+
+                genre = await repo.save(genre);
+
+                resolve(genre);
+            }
+            catch (err) {
+
+                reject(err);
+            }
+        });
     }
 }
